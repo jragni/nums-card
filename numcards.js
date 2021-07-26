0ae21aa9f27b39d815be492ce59d5419e23111a7
Loni <kuangloni@gmail.com>
2021-07-26 16:48:14 -0700

    finished number facts

--- numcards.js
+++ numcards.js
@@ -3,24 +3,62 @@
 // for reference http://numbersapi.com/24/trivia
 const BASE_URL = "http://numbersapi.com";
 
-async function getFacts(number) {
-  let params = {json:""} 
-  const r1 = axios.get( `${BASE_URL}/${number}/trivia`, params = params);
-  const r2 = axios.get( `${BASE_URL}/${number}/trivia`, params = params);
-  const r3 = axios.get( `${BASE_URL}/${number}/trivia`, params = params);
+// Part 1: Number Facts
 
-  let response = [await r1, await r2, await r3];
-  console.log(response)
-  return response;
+// 1.
+async function getFavNum(number) {
+  let params = {json:""} 
+  const response = axios.get( `${BASE_URL}/${number}/trivia`, params = params);
+
+  updateDOM(response);
 }
 
+// 2.
+async function getNumsFacts(nums) {
+  let batch = ""
+  for (let i = 0; i < nums.length; i++) {
+    if (i !== nums.length - 1) {
+      batch += nums[i] + ",";
+    } else {
+      batch += nums[i];
+    }
+  }
 
-async function updateDOM() {
-  const facts = await getFacts(23);
-  for( let fact of facts){
-    let $p = $('<p>').text(fact.data);
-    $('body').append($p);
+  let params = {json:""} 
+  const response = await axios.get( `${BASE_URL}/${batch}/trivia`, params = params);
+  console.log(response.data)
+
+  let numberFacts = Object.values(response.data);
+  
+  for (let fact of numberFacts) {
+    updateDOM(fact)
   }
 }
 
-updateDOM();
+
+// 3. 
+async function getFacts(number) {
+  let promises = [];
+
+  for (let i = 0; i < 4; i++) {
+    let params = {json:""} 
+    const r = axios.get( `${BASE_URL}/${number}/trivia`, params = params);
+    promises.push(r);
+  }
+
+  let results = await Promise.all(promises);
+
+  for (let result of results) {
+    updateDOM(result.data);
+  }
+}
+
+
+function updateDOM(result) {
+  let $p = $('<p>').text(result);
+  $('#results').append($p);
+}
+
+// getFacts(4);
+// getNumsFacts([11, 24, 88]);
+// getFacts(11);
